487d370f261ea235a549de1a15a49d447d615ad6
adelille982 <a.delille982@gmail.com>
2024-06-14 17:06:46 +0200

    CinéNetwork V1

---
+++ CinéNetwork.js
@@ -0,0 +1,35 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const curtainLeft = document.querySelector(".curtain.left");
+const curtainRight = document.querySelector(".curtain.right");
+const backgroundSlide = document.querySelector(".background-slide");
+
+function openCurtains() {
+    curtainLeft.style.width = "0%";
+    curtainRight.style.width = "0%";
+    setTimeout(() => {
+        backgroundSlide.style.display = "none";
+    }, 5000);
+}
+
+function initializeSlides() {
+    slides.forEach((slide) => {
+        slide.style.display = "block";
+        slide.style.opacity = 0;
+    });
+    slides[currentIndex].style.opacity = 1;
+}
+
+function changeSlide() {
+    const oldIndex = currentIndex;
+    currentIndex = (currentIndex + 1) % slides.length;
+
+    slides[oldIndex].style.opacity = 0;
+    slides[currentIndex].style.opacity = 1;
+}
+
+window.onload = () => {
+    initializeSlides();
+    openCurtains();
+    setInterval(changeSlide, 5000);
+};
